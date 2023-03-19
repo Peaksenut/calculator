1ebe742b047f50eb0b6c9c7249f125d8e2fcc562
PJ <96671928+Peaksenut@users.noreply.github.com>
2023-03-19 22:50:35 +0800

    refactor compute and display to affect each other
    -fix point button
    -remove equals button, to fix later

--- script.js
+++ script.js
@@ -3,32 +3,27 @@ const clearBtn = document.querySelector('#clear');
 const equalBtn = document.querySelector('#equals');
 let answerDisplay = document.querySelector('#answer-display');
 let display = document.querySelector('#display');
-let toDisplay = ''
+const digits = document.querySelectorAll('.btn .digit');
 
 //display
-let isStart = true;
-let isPlus = false;
-let isMinus = false;
 let operatorCount = 0;
+let isNum = false;
+let isFloat = false;
+let expression = ''
 
 //compute
-let isNegativeNumber = false;
-let isDigit = false;
 let currentAnswer = 0;
-let currentNumber = 1;
 let currentOperator = '';
-let number= '';
-let ab = [];
+let expr = [];
+let fAnswer = 0;
 
 let isEquals = false;
 
+clearBtn.addEventListener('click', clear);
+// equalBtn.addEventListener('click', displayAnswer);
+
 displayOperation();
 compute();
-clear();
-
-clearBtn.addEventListener('click', clear);
-equalBtn.addEventListener('click', displayAnswer);
-
 
 function add(a, b) {
   return a + b;
@@ -51,202 +46,140 @@ function operate(callback, a, b) {
 }
 
 
-function convertToNum (str) {
-  return Number(str);
-}
-
-
-function del(str, n = 1) { 
+function deleteOperator(str, n = 1) { 
   return str.slice(0, str.length - n);
 }
 
 
 function clear() {
-  isStart = true;
-  isMinus = false;
-  isPlus = false;
-  operatorCount = 0;
-
-  isNegativeNumber = false;
-  isDigit = false;
   currentAnswer = 0;
-  currentNumber = 0;
+  operatorCount = 0;
   currentOperator = '';
-  number = ''
-  ab = [];
-
+  isNum = false;
+  isFloat = false;
   isEquals = false;
 
-  toDisplay = '';
+  expression = '';
   display.innerText = '';
   answerDisplay.innerText = '';
 }
 
 
 function displayAnswer() {
-  if (ab.length > 0 && isDigit) {
-    let answer = convertToNum(answerDisplay.innerText);
-    clear();
-    isDigit = true;
-    isStart = false;
-    isEquals = true;
-    if (answer < 0) isNegativeNumber = true;
-    ab[0] = answer;
-    toDisplay += answer;
-    display.innerText = answer;
-  }
+  
 }
-
-
-// function del() {
-
-// }
 
 
 function compute() {
   buttons.forEach(btn => { btn.addEventListener('click', () => {
-    if (btn.id === 'minus' && !isNegativeNumber && !isDigit) {
-      isNegativeNumber = true;
-    } 
+    if (expression) {
+      expr = toArray(expression);
+      let len = expr.length - 1;
 
-    if (btn.className === 'btn digit' && !isEquals) {
-      isDigit = true;
-      number += btn.innerText;
-      currentNumber = convertToNum(number);
-
-      if (isNegativeNumber) {
-        currentNumber *= -1;
+      if (len >= 2) {
+        if (expr[len - 1] === '+') currentOperator = add;
+        if (expr[len - 1] === '-') currentOperator = subtract;
+        if (expr[len - 1] === '×') currentOperator = multiply;
+        if (expr[len - 1] === '÷') currentOperator = divide;
       }
 
-      if (ab.length >= 1) {
-        ab[1] = currentNumber;
+      if (len === 2) {
+        fAnswer = expr[0];
       }
-    }
 
-    if (btn.className === 'btn operators') {
-      isEquals = false;
-
-      if (btn.id === 'plus') currentOperator = add
-      if (btn.id === 'minus' && isDigit) currentOperator = subtract;
-      if (btn.id === 'multiply') currentOperator = multiply;
-      if (btn.id === 'divide') currentOperator = divide;
-
-      if (ab.length === 2) {
-        ab = [];
-        ab[0] = currentAnswer
+      if (btn.className === 'btn operators' && len > 2) {
+        fAnswer = currentAnswer;
       }
-      
-      if (btn.id != 'minus') {
-        isNegativeNumber = false;
 
-        if (ab.length === 0) {
-          ab[0] = currentNumber;
+      if (!isNaN(expr[len]) && len > 1) {
+        currentAnswer = operate(currentOperator, Number(fAnswer), Number(expr[len]));
+        console.log(currentOperator, Number(fAnswer), Number(expr[len]))
+        answerDisplay.innerText = currentAnswer;
+        if(currentAnswer === Infinity) {
+          answerDisplay.innerText = "HELLO!"
         }
       }
-
-      if (btn.id === 'minus' && isDigit) {
-        isNegativeNumber = false;
-        if (ab.length === 0) {
-          ab[0] = currentNumber;
-        }
-      }
-
-      isDigit = false;
-      number = '';
-      currentNumber = 0;
-    }
-
-    if (ab.length === 2 && !isEquals) {
-      currentAnswer = operate(currentOperator, ab[0], ab[1]);
-      answerDisplay.innerText = currentAnswer;
     }
   })})
+  
 }
 
 
 function displayOperation() {
   buttons.forEach(btn => { btn.addEventListener('click', () => {
-    //if isStart, can only enter digits
-    if(isStart) {
-      if (btn.id === '' || btn.id === 'minus' && !isMinus) {
-        isStart = false;
-        isMinus = false;
-        toDisplay += btn.innerText;
-        if (btn.id === 'minus') {
-          isStart = true;
-          isMinus = true;
-        }
-      } 
+    if (btn.className === 'btn digit') {
+      isNum = true;
+      operatorCount = 0;
+      expression += btn.innerText;
+    } 
 
-    } else { //can enter operators and the other numbers
-      if (btn.id === ''){ //if btn is digits, add toDisplay
-          isMinus = false;
-          operatorCount = 0;
-        
-        if (isEquals) {
-          clear();
-          isStart = false;
-          toDisplay += btn.innerText;
-        } else {
-          toDisplay += btn.innerText;
-        }
-      } 
-
-      //for changing operators
-      if (operatorCount === 1) {
-        if (btn.id === 'plus' || btn.id === 'multiply' || btn.id === 'divide') {
-          if (btn.id == 'plus') {
-            isPlus = true;
-          } else {
-            isPlus = false
-            isMinus = false;
-          }
-          toDisplay = del(toDisplay);
-          operatorCount = 1;
-          toDisplay += `${btn.innerText}`;
-        } else if (btn.id === 'minus' && isPlus) { //changing plus with minus
-          toDisplay = del(toDisplay);
-          operatorCount = 1;
-          toDisplay += `${btn.innerText}`;
-        }
-      } else if (operatorCount === 2) { //consecutive symbols: /- and *-
-        if (btn.id === 'plus' || btn.id === 'multiply' || btn.id === 'divide') {
-          toDisplay = del(toDisplay, 2)
-          operatorCount = 1;
-          isMinus = false;
-          toDisplay += `${btn.innerText}`;
-        }
-      } 
-
-      if (operatorCount === 1 && !isPlus && !isMinus) { //to allow minus after / and *
-        if (btn.id === 'minus') {
-          isMinus = true;
-          operatorCount = 2;
-          toDisplay += `${btn.innerText}`;
-        }
+    if (btn.id === 'point' && !isFloat) {
+      if (isNaN(expression.slice(-1)) || !expression.slice(-1)) {
+        expression += '0.';
+      } else {
+        expression += btn.innerText;
       }
+      isNum = true;
+      isFloat = true;
+      operatorCount = 0;
+    }
+    
 
-      if (operatorCount === 0) {
-        if (btn.id === 'plus' || btn.id === 'multiply' || btn.id === 'divide') {
-          if (btn.id === 'plus'){
-            isPlus = true;
-          } else {
-            isPlus = false;
-            isMinus = false;
-          }
-          operatorCount = 1;
-          toDisplay += `${btn.innerText}`;
-        } else if (btn.id === 'minus') {  
-          isPlus = false;
-          isMinus = true;
-          operatorCount = 1;;
-          toDisplay += `${btn.innerText}`;
-        }
+    if (btn.className === 'btn operators' && operatorCount === 0) {
+      if (expression.endsWith('.')) expression += '0';
+      if (isNum) {
+        isNum = false;
+        isFloat = false;
+        operatorCount = 1;
+        expression += btn.innerText;
+      } else if (!isNum && btn.id === 'minus') {
+        isNum = true;
+        isFloat = false;
+        operatorCount = 1;
+        expression += btn.innerText;
       }
     }
 
-    if (toDisplay) {
-      display.innerText = toDisplay;
+    // multiplying or dividing negative numbers
+    if (btn.id === 'minus') {
+      if (expression.endsWith('÷') || expression.endsWith('×')) {
+        operatorCount = 2;
+        expression += btn.innerText;
+      }
     }
+    
+    //replacing operators
+    if (!isNum && operatorCount === 1) {
+      if (btn.id === 'minus' && expression.endsWith('+')) {
+        expression = expression.slice(0, -1);
+        expression += btn.innerText;
+      } else if (btn.className === 'btn operators') {
+        expression = expression.slice(0, -1);
+        expression += btn.innerText;
+      }
+    }
+
+    if (btn.className === 'btn operators' && btn.id != 'minus' && operatorCount === 2) {
+      if (expression.endsWith('×-') || expression.endsWith('÷-')) {
+        expression = expression.slice(0, -2);
+        expression += btn.innerText;
+        operatorCount = 1;
+      }
+    }
+    //
+
+    if (btn.id === 'delete') deleteChar();
+    
+    display.innerText = expression;
   })})
+}
+
+//format expression, convert the element if element is a number
+//display the formatted expression using join
+function toArray(expression) {
+  return expression.match(/(?:(?<![\d.])-)?\d+(?:\.\d+)?|[÷×+-]/g);
+}
+
+function deleteChar() {
+  expression = expression.slice(0, -1);
 }
